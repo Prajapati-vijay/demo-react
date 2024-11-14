e317c6e6fc5c117d1441f180acb6c86c81d446f1
vijay-prajapati <vijayprajapati260263@gmail.com>
2024-11-14 17:49:37 +0530

    Update student.js

--- src/components/student.js
+++ src/components/student.js
@@ -7,7 +7,7 @@ const Students = () => {
 
   useEffect(() => {
     // Fetch data from FastAPI backend
-    fetch('http://djr.local/fast2/students')
+    fetch('http://djr.local/flask/students')
       .then(response => response.json())
       .then(data => setStudents(data))
       .catch(err => setError(err.message));
@@ -15,7 +15,7 @@ const Students = () => {
 
   return (
     <div>
-      <h1>Students data from Fast API</h1>
+      <h1>Students data from Flask API</h1>
       {error && <p>Error: {error}</p>}
       <ul>
         {students.map(student => (
