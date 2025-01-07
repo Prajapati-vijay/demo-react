c58dc35f73a2d578b0b2899d82f6fc0c7ba688ed
Vijay <vijay@finflock.com>
2025-01-07 13:05:08 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Students from './components/student';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/home";
+import About from "./components/about";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World from React
-        </p>
-        <Students />
-      </header>
-      
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
