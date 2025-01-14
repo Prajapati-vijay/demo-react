96e3f63b5ff5c5bf0d21c03d41b5e32528c84494
vijay-prajapati <vijayprajapati260263@gmail.com>
2025-01-14 18:31:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import Students from "./components/student";
+import CookieReader from "./components/CookieReader";
 
 function App() {
   return (
@@ -18,12 +19,16 @@ function App() {
     <li>
             <Link to="/student">Student Data</Link>
           </li>
+    <li>
+            <Link to="/cookie">Cookie Data</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
        <Route path="/student" element={<Students />} />
+      <Route path="/cookie" element={<CookieReader />} />
       </Routes>
     </Router>
   );
