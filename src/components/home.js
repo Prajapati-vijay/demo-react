4cfd056ca9c0cb32d1ea0d1e5ab0a953724cf5b5
vijay-prajapati <vijayprajapati260263@gmail.com>
2026-03-06 19:09:24 +0530

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to the Edited Home Page by QCH</h1>;
+  return <h1>Welcome to the Edited Home Page by QCH V2</h1>;
 };
 
 export default Home;
